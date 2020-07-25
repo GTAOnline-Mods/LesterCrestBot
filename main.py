ccf4ffe1f4240a04ff2de27b2654b24e6b3b23c5
Dan6erbond <moravrav@gmail.com>
2020-07-25 10:28:39 +0200

    Implement basic bot

---
+++ main.py
@@ -0,0 +1,127 @@
+import configparser
+
+import apraw
+import banhammer
+import discord
+from banhammer.models import Subreddit
+from discord.ext import commands
+
+from config import config as lc_config
+
+bot = commands.Bot(
+    bh_config["command_prefix"],
+    description="/r/gtaonline's moderation bot using Banhammer.py.")
+
+bh = banhammer.Banhammer(
+    apraw.Reddit("LCB"),
+    bot=bot,
+    change_presence=bh_config["change_presence"])
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    print(error)
+
+
+@bot.event
+async def on_ready():
+    print(str(bot.user) + ' is running.')
+
+    for sub in bh_config["subreddits"]:
+        s = Subreddit(bh, **sub)
+        await s.load_reactions()
+        await bh.add_subreddits(s)
+
+    bh.run()
+
+
+@bot.command()
+async def subreddits(ctx):
+    await ctx.send(embed=bh.get_subreddits_embed())
+
+
+@bot.command()
+async def reactions(ctx):
+    await ctx.send(embed=bh.get_reactions_embed())
+
+
+@bh.new()
+async def handle_new(p):
+    msg = await bot.get_channel(bh_config["new_channel"]).send(embed=await p.get_embed())
+    for react in p.get_reactions():
+        await msg.add_reaction(react.emoji)
+
+
+@bh.comments()
+async def handle_comments(p):
+    msg = await bot.get_channel(bh_config["comments_channel"]).send(embed=await p.get_embed())
+    for react in p.get_reactions():
+        await msg.add_reaction(react.emoji)
+
+
+@bh.mail()
+async def handle_mail(p):
+    msg = await bot.get_channel(bh_config["mail_channel"]).send(embed=await p.get_embed())
+    for react in p.get_reactions():
+        await msg.add_reaction(react.emoji)
+
+
+@bh.queue()
+async def handle_queue(p):
+    msg = await bot.get_channel(bh_config["queue_channel"]).send(embed=await p.get_embed())
+    for react in p.get_reactions():
+        await msg.add_reaction(react.emoji)
+
+
+@bh.reports()
+async def handle_reports(p):
+    msg = await bot.get_channel(bh_config["reports_channel"]).send(embed=await p.get_embed())
+    for react in p.get_reactions():
+        await msg.add_reaction(react.emoji)
+
+
+@bh.mod_actions()
+async def handle_actions(p):
+    msg = await bot.get_channel(bh_config["actions_channel"]).send(embed=await p.get_embed())
+    for react in p.get_reactions():
+        await msg.add_reaction(react.emoji)
+
+
+@bot.event
+async def on_message(m):
+    if m.author.bot:
+        return
+
+    item = await bh.get_item(m.content)
+    if item:
+        for react in item.get_reactions():
+            await m.add_reaction(react.emoji)
+
+    await bot.process_commands(m)
+
+
+@bot.event
+async def on_raw_reaction_add(p):
+    c = bot.get_channel(p.channel_id)
+
+    u = c.guild.get_member(p.user_id)
+    if u.bot:
+        return
+
+    m = await c.fetch_message(p.message_id)
+    e = p.emoji.name if not p.emoji.is_custom_emoji() else f"<:{p.emoji.name}:{p.emoji.id}>"
+
+    item = await bh.get_item(m.embeds[0] if not m.embeds else m.content)
+    if not item:
+        return
+
+    result = await item.get_reaction(e).handle(item, user=u.nick)
+    channel = bot.get_channel(lc_config["approved_channel"] if result.approved else lc_config["removed_channel"])
+    await channel.send(await result.get_message())
+
+    await m.delete()
+
+
+config = configparser.ConfigParser()
+config.read("discord.ini")
+bot.run(config["LCB"]["token"])
