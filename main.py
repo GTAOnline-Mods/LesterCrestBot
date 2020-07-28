6dfe7728a6db2fe8e031a37d5b2e1ffcfed15f60
Dan6erbond <moravrav@gmail.com>
2020-07-28 18:48:24 +0200

    Make bot LesterCrest class

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import apraw
 import banhammer
 import discord
-from banhammer.models import Subreddit
+from banhammer.models import RedditItem, Subreddit
 from discord.ext import commands
 
 from cmds import HelpCommand
@@ -23,194 +23,150 @@ fileHandle.setFormatter(formatter)
 fileHandle.setLevel(logging.WARNING)
 logger.addHandler(fileHandle)
 
+reddit = apraw.Reddit("LCB")
 gta_green = discord.Colour(0).from_rgb(207, 226, 206)
 
-bot = commands.Bot(
-    lc_config["command_prefix"],
-    description="/r/gtaonline's moderation bot using Banhammer.py.",
-    help_command=HelpCommand(gta_green))
 
-bh = banhammer.Banhammer(
-    apraw.Reddit("LCB"),
-    bot=bot,
-    change_presence=lc_config["change_presence"],
-    embed_color=gta_green,
-    message_builder=MessageBuilder())
+class LesterCrest(commands.Bot, banhammer.Banhammer):
+    def __init__(self, **options):
+        super().__init__(lc_config["command_prefix"], help_command=HelpCommand(gta_green),
+                         description="/r/gtaonline's moderation bot using Banhammer.py.", **options)
+        banhammer.Banhammer.__init__(self, reddit, bot=self, embed_color=gta_green,
+                                     change_presence=lc_config["change_presence"])
+
+    async def on_command_error(self, ctx: commands.Context, error):
+        if isinstance(error, discord.ext.commands.errors.CommandNotFound):
+            pass
+        else:
+            print(error)
+
+    async def on_ready(self):
+        print(f"{self.user} is running.")
+
+        for sub in lc_config["subreddits"]:
+            s = Subreddit(self, **sub)
+            await s.load_reactions()
+            await self.add_subreddits(s)
+
+        channel = self.get_channel(734713971428425729)
+        message = await channel.fetch_message(736613065889546321)
+
+        for sub in self.subreddits:
+            embed = await sub.get_reactions_embed(embed_template=lester.embed)
+            await message.edit(embed=embed)
+            break
+
+        self.run()
+
+    async def on_message(self, message: discord.Message):
+        if m.author.self:
+            return
+
+        item = await self.get_item(message.content)
+        if item:
+            await item.add_reactions(message)
+
+        await self.process_commands(message)
+
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        c = self.get_channel(payload.channel_id)
+
+        u = c.guild.get_member(payload.user_id)
+        if u.bot:
+            return
+        if not any(role.id == 734714209342062602 for role in u.roles):
+            return
+
+        m = await c.fetch_message(payload.message_id)
+        e = payload.emoji.name if not payload.emoji.is_custom_emoji() else f"<:{payload.emoji.name}:{payload.emoji.id}>"
+
+        item = await self.get_item(m.embeds[0] if m.embeds else m.content)
+        if not item:
+            return
+
+        reaction = item.get_reaction(e)
+        if not reaction:
+            return
+
+        await m.delete()
+        result = await reaction.handle(item, user=u.nick)
+        channel = self.get_channel(lc_config["approved_channel"] if result.approved else lc_config["removed_channel"])
+        await channel.send(embed=await result.get_embed(embed_template=self.embed))
+
+        with open(lc_config["payloads_file"], "ab+") as f:
+            pickle.dump(result.to_dict(), f)
+
+    @commands.command()
+    async def reload(self, ctx: commands.Context):
+        await ctx.message.delete()
+        for sub in self.subreddits:
+            await sub.load_reactions()
+        await ctx.send("Reloaded all subreddit reactions!", delete_after=3)
+
+        channel = self.get_channel(734713971428425729)
+        message = await channel.fetch_message(736613065889546321)
+        for sub in self.subreddits:
+            embed = await sub.get_reactions_embed(embed_template=self.embed)
+            await message.edit(embed=embed)
+            break
+
+    @commands.command()
+    async def subreddits(self, ctx: commands.Context):
+        await ctx.send(embed=self.get_subreddits_embed(embed_template=self.embed))
+
+    @commands.command()
+    async def reactions(self, ctx: commands.Context):
+        await ctx.send(embed=self.get_reactions_embed(embed_template=self.embed))
+
+    @property
+    def embed(self):
+        embed = discord.Embed(colour=gta_green)
+        embed.set_footer(text="Lester Crest Bot", icon_url=self.user.avatar_url)
+        embed.timestamp = datetime.now()
+        return embed
 
 
-def get_embed():
-    embed = discord.Embed(colour=gta_green)
-    embed.set_footer(text="Lester Crest Bot", icon_url=bot.user.avatar_url)
-    embed.timestamp = datetime.now()
-    return embed
+lester = LesterCrest()
 
 
-async def get_reactions_embed():
-    embed = get_embed()
-
-    for sub in bh.subreddits:
-        subreddit = await sub.get_subreddit()
-        embed.set_author(
-            name=f"/r/{sub} Configured Reactions",
-            url=f"https://www.reddit.com/r/{sub}/wiki/banhammer-reactions",
-            icon_url=subreddit.community_icon)
-        fields = list()
-        for reaction in sub.reactions:
-            text = repr(reaction).replace(str(reaction) + " | ", "")
-            if reaction.reply:
-                text.replace(" | reply", "")
-                text += f"\n\n**Reply**\n>>> {reaction.reply}"
-                fields.append({"name": str(reaction), "value": text})
-            else:
-                fields = [{"name": str(reaction), "value": text}, *fields]
-        for field in fields:
-            embed.add_field(**field)
-        break
-
-    return embed
+@lester.new()
+async def handle_new(item: RedditItem):
+    embed = await item.get_embed(embed_template=lester.embed)
+    msg = await lester.get_channel(lc_config["new_channel"]).send(embed=embed)
+    await item.add_reactions(msg)
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, discord.ext.commands.errors.CommandNotFound):
-        pass
-    else:
-        print(error)
-
-
-@bot.event
-async def on_ready():
-    print(str(bot.user) + ' is running.')
-
-    for sub in lc_config["subreddits"]:
-        s = Subreddit(bh, **sub)
-        await s.load_reactions()
-        await bh.add_subreddits(s)
-
-    channel = bot.get_channel(734713971428425729)
-    message = await channel.fetch_message(736613065889546321)
-    await message.edit(embed=await get_reactions_embed())
-
-    bh.run()
-
-
-@bot.command()
-async def reload(ctx):
-    await ctx.message.delete()
-    for sub in bh.subreddits:
-        await sub.load_reactions()
-    await ctx.send("Reloaded all subreddit reactions!", delete_after=3)
-
-    channel = bot.get_channel(734713971428425729)
-    message = await channel.fetch_message(736613065889546321)
-    await message.edit(embed=await get_reactions_embed())
-
-
-@bot.command()
-async def subreddits(ctx):
-    await ctx.send(embed=bh.get_subreddits_embed())
-
-
-@bot.command()
-async def reactions(ctx):
-    await ctx.send(embed=bh.get_reactions_embed())
-
-
-@bh.new()
-async def handle_new(p):
-    msg = await bot.get_channel(lc_config["new_channel"]).send(embed=await p.get_embed())
-    for react in p.get_reactions():
-        await msg.add_reaction(react.emoji)
-
-
-@bh.comments()
-async def handle_comments(p):
-    author_name = await p.get_author_name()
+@lester.comments()
+async def handle_comments(item: RedditItem):
+    author_name = await item.get_author_name()
     if author_name.lower() in ["automoderator", "lestercrestbot"]:
         return
-    msg = await bot.get_channel(lc_config["comments_channel"]).send(embed=await p.get_embed())
-    for react in p.get_reactions():
-        await msg.add_reaction(react.emoji)
+    embed = await item.get_embed(embed_template=lester.embed)
+    msg = await lester.get_channel(lc_config["comments_channel"]).send(embed=embed)
+    await item.add_reactions(msg)
 
 
-@bh.mail()
-async def handle_mail(p):
-    msg = await bot.get_channel(lc_config["mail_channel"]).send(embed=await p.get_embed())
-    for react in p.get_reactions():
-        await msg.add_reaction(react.emoji)
+@lester.mail()
+async def handle_mail(item: RedditItem):
+    embed = await item.get_embed(embed_template=lester.embed)
+    msg = await lester.get_channel(lc_config["mail_channel"]).send(embed=embed)
+    await item.add_reactions(msg)
 
 
-@bh.queue()
-async def handle_queue(p):
-    msg = await bot.get_channel(lc_config["queue_channel"]).send(embed=await p.get_embed())
-    for react in p.get_reactions():
-        await msg.add_reaction(react.emoji)
+@lester.reports()
+async def handle_reports(item: RedditItem):
+    embed = await item.get_embed(embed_template=lester.embed)
+    msg = await lester.get_channel(lc_config["reports_channel"]).send(embed=embed)
+    await item.add_reactions(msg)
 
 
-@bh.reports()
-async def handle_reports(p):
-    msg = await bot.get_channel(lc_config["reports_channel"]).send(embed=await p.get_embed())
-    for react in p.get_reactions():
-        await msg.add_reaction(react.emoji)
-
-
-@bh.mod_actions()
-async def handle_actions(p):
-    msg = await bot.get_channel(lc_config["actions_channel"]).send(embed=await p.get_embed())
-    for react in p.get_reactions():
-        await msg.add_reaction(react.emoji)
-
-
-@bot.event
-async def on_message(m):
-    if m.author.bot:
-        return
-
-    item = await bh.get_item(m.content)
-    if item:
-        for react in item.get_reactions():
-            await m.add_reaction(react.emoji)
-
-    await bot.process_commands(m)
-
-
-@bot.event
-async def on_raw_reaction_add(p):
-    c = bot.get_channel(p.channel_id)
-
-    u = c.guild.get_member(p.user_id)
-    if u.bot:
-        return
-    if not any(role.id == 734714209342062602 for role in u.roles):
-        return
-
-    m = await c.fetch_message(p.message_id)
-    e = p.emoji.name if not p.emoji.is_custom_emoji() else f"<:{p.emoji.name}:{p.emoji.id}>"
-
-    item = await bh.get_item(m.embeds[0] if m.embeds else m.content)
-    if not item:
-        return
-
-    reaction = item.get_reaction(e)
-    if not reaction:
-        return
-
-    await m.delete()
-    result = await reaction.handle(item, user=u.nick)
-    channel = bot.get_channel(lc_config["approved_channel"] if result.approved else lc_config["removed_channel"])
-    await channel.send(embed=await result.get_embed())
-
-    with open(lc_config["payloads_file"], "ab+") as f:
-        pickle.dump({
-            "item": result.item.url,
-            "user": result.user,
-            "actions": result.actions,
-            "approved": result.approved,
-            "reply": result.reply,
-            "emoji": result.emoji
-        }, f)
+@lester.mod_actions()
+async def handle_actions(item: RedditItem):
+    embed = await item.get_embed(embed_template=lester.embed)
+    msg = await lester.get_channel(lc_config["actions_channel"]).send(embed=embed)
+    await item.add_reactions(msg)
 
 
 config = configparser.ConfigParser()
 config.read("discord.ini")
-bot.run(config["LCB"]["token"])
+lester.run(config["LCB"]["token"])
