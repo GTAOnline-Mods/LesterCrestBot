3b921a3e34c00f53b0e61a6c53725546df96f8a9
Dan6erbond <moravrav@gmail.com>
2020-07-31 13:25:01 +0200

    Add bad word + tests

--- dirty_words.py
+++ dirty_words.py
@@ -1,6 +1,7 @@
 import json
 
-if __name__ == "__main__":
+
+def write_words():
     ws = list()
     with open("assets/DirtyWords.json", encoding="utf8") as f:
         words = json.load(f)["RECORDS"]
@@ -10,3 +11,11 @@ if __name__ == "__main__":
                 ws.append(word["word"])
     with open("assets/DirtyWords_en.txt", "w+") as f:
         f.write("\n".join(ws))
+
+
+if __name__ == "__main__":
+    with open("assets/DirtyWords_en.txt") as f:
+        words = f.read().splitlines()
+        texts = ["CausticPenguino is a butt LMFAO.", "Mods are gay.", "Badger is a motherfucking cocksucker."]
+        for text in texts:
+            print(text, any(word in text for word in words))
