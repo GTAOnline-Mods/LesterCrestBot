f7d0e30647e93665883204594d6f096e2f267d0d
Dan6erbond <moravrav@gmail.com>
2020-07-31 12:43:44 +0200

    Fix text

--- lester.py
+++ lester.py
@@ -104,7 +104,7 @@ class LesterCrest(Bot, Banhammer):
             msg = f"The submission by /u/{await item.get_author_name()} was already approved by /u/{item.item.approved_by}, are you sure you want to remove it?\n\n" \
                 f"{item.url}"
         elif getattr(item.item, "removed_by_category", None) == "moderator" and reaction.approve and item.item.removed_by.lower() != "automoderator":
-            msg = f"The submission by /u/{await item.get_author_name()} was already approved by /u/{item.item.removed_by}, are you sure you want to approve it?\n\n" \
+            msg = f"The submission by /u/{await item.get_author_name()} was already removed by /u/{item.item.removed_by}, are you sure you want to approve it?\n\n" \
                 f"{item.url}"
 
         if msg:
